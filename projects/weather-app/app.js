320ad51968295f7cc54127b09b474fd74dfbbfe3
Nitish Bhushan <nitishbhushan2013@gmail.com>
2018-07-03 08:23:23 +1000

    ajax call chaining

--- projects/weather-app/app.js
+++ projects/weather-app/app.js
@@ -23,13 +23,18 @@ geocode.geocodeAddress(argv.address, (error, results )=>{
     if(error)
         console.log(error);
     else {    
-       // console.log(`details are ${JSON.stringify(results,undefined, 4)}`)
-       latitude = results.latitude;
-       longitude = results.longitude;
        console.log(`latitude is ${latitude} and longitude is ${longitude}`);
-    }    
-})
-
+       // calling forcase.io API to get the currebt day weather report
+       forcast.getCurrentWeatherDetails(results.latitude, results.longitude, (error, response) => {
+            if(error)
+                console.log(error);
+            else 
+                console.log(`Weather report--> ${JSON.stringify(response, undefined, 4)}`)
+        })
+    } 
+})       
+// first approach : Ajax call chaining. rather primitive and error prone solution
+/*
 setTimeout(() => {
     forcast.getCurrentWeatherDetails(latitude, longitude, (error, response) => {
         if(error)
@@ -40,4 +45,4 @@ setTimeout(() => {
             
     })
 }, 2000); // wait for 2 secs to get the latitude and longitude
-
+*/
