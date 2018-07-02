3f03e17474e09e0bcdfdc720f594e53a979b4130
Nitish Bhushan <nitishbhushan2013@gmail.com>
2018-07-03 07:10:06 +1000

    modified version using callback approach

--- projects/weather-app/app.js
+++ projects/weather-app/app.js
@@ -1,4 +1,23 @@
 const geocode = require("./geocode/geocode")
+const yargs = require("yargs");
 
-var geocodeLocation = geocode.geocodeAddress();
-console.log(`geocodeLocation---> ${JSON.stringify(geocodeLocation)}`);
+//define yargs 
+const argv = yargs
+.options({
+    a :{
+        dvaremand : true,
+        alias : 'address',
+        string : true,
+        describe : 'address to get the weather report'
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+geocode.geocodeAddress(argv.address, (error, results )=>{
+    if(error)
+        console.log(error);
+    else    
+        console.log(`details are ${JSON.stringify(results,undefined, 4)}`)
+})
--- projects/weather-app/geocode/geocode.js
+++ projects/weather-app/geocode/geocode.js
@@ -1,24 +1,10 @@
 // required modules
 const request = require("request");
-const yargs = require("yargs");
-
-//define yargs 
-const argv = yargs
-.options({
-    a :{
-        dvaremand : true,
-        alias : 'address',
-        string : true,
-        describe : 'address to get the weather report'
-    }
-})
-.help()
-.alias('help', 'h')
-.argv;
 
 
-var geocodeAddress = () => {
-    var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`;
+
+var geocodeAddress = (address, callback) => {
+    var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}`;
     console.log(`url --> ${url}`);
     //create request and get the address and set geocodeAddress
     request({
@@ -27,14 +13,17 @@ var geocodeAddress = () => {
         },
         (error, response, body) => {
             if(error) {
-                console.log("could not connect to Google Server")
+                callback("could not connect to Google Server")
             }else if(body.status === 'ZERO_RESULTS') {
-                console.log("unable to find the address");
+                callback("unable to find the address");
             }    
             else if (body.status === 'OK') {
                 console.log(body.status )
-                console.log(body.results[0].geometry.location)
-                return body.results[0].geometry.location;
+                callback(undefined,{
+                    address :  body.results[0].formatted_address,
+                    latitude: body.results[0].geometry.location.lat,
+                    longitude: body.results[0].geometry.location.lng 
+                });
             }
     })
 }
